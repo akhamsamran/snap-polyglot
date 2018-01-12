aabc2f0021699357ebb04fd83d4161231363719f
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-12 08:41:19 -0700

    added aceK clubs

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
 
 	</head>
 	<body>
+		<!--http://sites.psu.edu/symbolcodes/codehtml/#using-->
 		<p>&iquest;Qi&eacute;n fue unos de los poetas m&aacute;s importantes en la historia de M&eacute;xico? Sor Juana In&eacute;s de la Cruz. Uno de sus eleg&iacute;as m&aacute;s importantes fue &laquo;De la beldad de Laura enamorados&raquo;.</p>
 		<p>Who was one of the most important poets in the history of Mexico. Sor Juana In&eacute;s  de la Cruz. One of her most imporant selections was &laquo;De la beldad de Laura enamorados&raquo;.</p>
+		<p>A&clubs; K&clubs;</p>
 
 	</body>
 </html>
