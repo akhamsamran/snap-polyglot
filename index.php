e642c640b39d9ebc43d33d4dd314d41f5b876952
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-12 08:26:38 -0700

    added some text with Spanish accents

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>snap-polyglot</title>
+
+	</head>
+	<body>
+		<p>&iquest;Qi&eacute;n fue unos de los poetas m&aacute;s importantes en la historia de M&eacute;xico? Sor Juana In&eacute;s de la Cruz. Uno de sus eleg&iacute;as m&aacute;s importantes fue &laquo;De la beldad de Laura enamorados&raquo;.</p>
+		<p>Who was one of the most important poets in the history of Mexico. Sor Juana In&eacute;s  de la Cruz. One of her most imporant selections was &laquo;De la beldad de Laura enamorados&raquo;.</p>
+
+	</body>
+</html>
